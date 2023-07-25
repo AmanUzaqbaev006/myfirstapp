febccac037447429c8650db5b09862557bd4f754
AmanUzaqbaev006 <140445591+AmanUzaqbaev006@users.noreply.github.com>
2023-07-25 09:28:14 +0300

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -1,13 +1,6 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
 
 import os
+import sys
 
 from django.core.asgi import get_asgi_application
 
